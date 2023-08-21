b26d4f04481e2f785ac600416e0ce2a3f0cb9162
Benayaram Rekha <benayaramcraetions@gmail.com>
2023-08-21 23:23:25 +0530

    Navbar Changed

--- client/src/assets/components/navbar/navbar.js
+++ client/src/assets/components/navbar/navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import './navbar.css'; // Make sure to import the corresponding CSS file
+import './navbar.css';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar blue">
       <div className="navbar-container">
-        <div className="brand">
-          Your Brand Name
-        </div>
+        <div className="brand">K-Hub React Practice App</div>
+        <div className="nav-blocks">
+        <div className="nav-block blue"></div>
+        <div className="nav-block navy"></div>
+      </div>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
