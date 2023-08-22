27493c42d473b88359c758dcd38aaaee3d40d018
Benayaram Rekha <benayaramcraetions@gmail.com>
2023-08-22 11:05:55 +0530

    Home page Changed

--- client/src/assets/components/Home/Home.js
+++ client/src/assets/components/Home/Home.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import './Home.css'; // Make sure to import the corresponding CSS file
+import './Home.css'; // Make sure to create this CSS file for styling
 
-const Home = () => {
+function HomePage() {
   return (
-    <div className="home-container">
-      <div className="image-container">
-        
+    <div className="home-page">
+      <div className="left-content">
+        <div className="grid-container">
+          <div className="grid-item">Item 1</div>
+          <div className="grid-item">Item 2</div>
+          <div className="grid-item">Item 3</div>
+          <div className="grid-item">Item 4</div>
+          <div className="grid-item">Item 5</div>
+          <div className="grid-item">Item 6</div>
+        </div>
       </div>
-      <div className="content">
-        <h1>Welcome to Our Website</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper libero eget ligula ultrices, vel euismod justo venenatis.</p>
-        <button className="cta-button">Learn More</button>
+      <div className="right-content">
+        <div className="card">
+          <h2>About the Data</h2>
+          <p>Write ~150 to 200 word introduction to your chosen data set</p>
+        </div>
       </div>
     </div>
   );
-};
+}
 
-export default Home;
+export default HomePage;
