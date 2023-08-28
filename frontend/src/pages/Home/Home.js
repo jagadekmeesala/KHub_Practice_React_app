fc11d24b64c4eab968f6113d22bad53c87bd1d7a
Benayaram Rekha <benayaramcraetions@gmail.com>
2023-08-28 15:50:38 +0530

    Home page Changed

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,52 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 import './Home.css';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
-function Home() {
-  return (
-    <div className="home-page">
-      <div className="left-content">
-        <div className="grid-container">
-          <div className="grid-item">
-            <img src="" alt="Mean" />
-          </div>
-          <div className="grid-item">
-            <img src="" alt="Median" />
-          </div>
-          <div className="grid-item">
-            <img src="" alt="Mode" />
-          </div>
-          <div className="grid-item">
-            <img src="" alt="SD" />
-          </div>
-          <div className="grid-item">
-            <img src="" alt="Max" />
-          </div>
-          <div className="grid-item">
-            <img src="" alt="Min" />
-          </div>
+const Home = () => {
+  const [statistics, setStatistics] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/stats", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data)
+        setStatistics(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const renderGradientBar = (statistic) => {
+    const stats = statistics[statistic];
+    const data = Object.keys(stats).map((attribute) => ({
+      name: attribute,
+      value: stats[attribute],
+    }));
+    
+    const customColorRGB = 'rgb(255, 165, 0)';
+    
+    return (
+      <div className="box" key={statistic}>
+        <h3>{statistic}</h3>
+        <div className="chart">
+          <BarChart width={185.56} height={200} data={data}>
+            <XAxis dataKey="name" tick={{ fill: 'white' }} />
+            <YAxis tick={{ fill: 'white' }} />
+            <Tooltip />
+            <Bar dataKey="value" fill={customColorRGB} />
+          </BarChart>
         </div>
       </div>
-      <div className="right-content">
-        <div className="card">
-          <div className="card-content">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="10"
-              height="10"
-              viewBox="0 0 448 512"
-              className="svg-icon"
-            >
-              {/* SVG path here */}
-            </svg>
-            <div className="card-text">
-              <h2>About the Data</h2>
-            </div>
-          </div>
-          <p>Write ~150 to 200 word introduction to your chosen data set</p>
-        </div>
+    );
+  };
+
+  return (
+    <div className="layout-container">
+      <div className="left-half">
+        {Object.keys(statistics).map((statistic) => renderGradientBar(statistic))}
+      </div>
+      <div className="right-half">
+        
+        
+        
+        <p className="textarea" placeholder="Enter up to 150 words">
+          
+          <FontAwesomeIcon icon={faDatabase} size="2x" />
+
+          About the data
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
